c6e8176abe630eba871be8f1910248e9eb470023
tav <tav@espians.com>
2014-03-16 23:00:14 +0000

    python: implement syntax.highlight and python api framework.

---
+++ python/api.py
@@ -0,0 +1,81 @@
+# Public Domain (-) 2014 The Wikifactory Authors.
+# See the Wikifactory UNLICENSE file for details.
+
+from json import dumps as encode_json
+from secret import APP_ID, TOKEN
+from weblite import app, handle, RUNNING_ON_GOOGLE_SERVERS
+
+from pygments import highlight
+from pygments.formatters import HtmlFormatter
+from pygments.lexers import get_lexer_by_name, TextLexer
+
+from tavutil.crypto import secure_string_comparison
+
+# -----------------------------------------------------------------------------
+# Globals
+# -----------------------------------------------------------------------------
+
+APIS = {}
+
+PARAM_NONE = 0
+PARAM_SINGLE = 1
+PARAM_SPLAT = 2
+
+# -----------------------------------------------------------------------------
+# API Handler
+# -----------------------------------------------------------------------------
+
+@handle('api', json='data')
+def api_handler(ctx, method, token, data):
+    # Check that the X-Appengine-Inbound-Appid header matches.
+    if RUNNING_ON_GOOGLE_SERVERS:
+        if ctx.environ.get('HTTP_X_APPENGINE_INBOUND_APPID') != APP_ID:
+            raise ctx.NotFound
+    # Check that the shared secret token matches.
+    if not secure_string_comparison(token, TOKEN):
+        raise ctx.NotFound
+    ctx.response_headers['Content-Type'] = 'application/json'
+    try:
+        handler, param = APIS[method]
+        if param == PARAM_SPLAT:
+            reply = handler(ctx, **data)
+        elif param == PARAM_SINGLE:
+            reply = handler(ctx, data)
+        else:
+            reply = handler(ctx)
+        return encode_json({'reply': reply})
+    except Exception, err:
+        return encode_json({'error':
+            {'type': err.__class__.__name__, 'message': str(err)}
+            })
+
+# -----------------------------------------------------------------------------
+# API Decorator
+# -----------------------------------------------------------------------------
+
+def api(param=PARAM_SPLAT):
+    def __register(handler):
+        name = handler.__name__.replace('_', '.')
+        APIS[name] = (handler, param)
+        return handler
+    return __register
+
+# -----------------------------------------------------------------------------
+# API Methods
+# -----------------------------------------------------------------------------
+
+@api()
+def syntax_highlight(ctx, text, lang=None):
+    if lang:
+        try:
+            lexer = get_lexer_by_name(lang)
+        except ValueError:
+            lang = 'txt'
+            lexer = TextLexer()
+    else:
+        lang = 'txt'
+        lexer = TextLexer()
+    formatter = HtmlFormatter(
+        cssclass='syntax %s' % lang, lineseparator='<br/>'
+        )
+    return highlight(text, lexer, formatter)
